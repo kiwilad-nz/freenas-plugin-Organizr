f12dd158d233b71b3b6e861d42877244d5b82a4f
kiwilad-nz <50952231+kiwilad-nz@users.noreply.github.com>
2019-05-24 03:25:50 +1200

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
-# Create directories and links for mount points etc.
-mkdir -p /config /downloads
-ln -s /usr/local/bin/python2.7 /usr/bin/python
-ln -s /usr/local/bin/python2.7 /usr/bin/python2
+# Additional Config
+sed -i '' -e 's?listen = 127.0.0.1:9000?listen = /var/run/php-fpm.sock?g' /usr/local/etc/php-fpm.d/www.conf
+sed -i '' -e 's/;listen.owner = www/listen.owner = www/g' /usr/local/etc/php-fpm.d/www.conf
+sed -i '' -e 's/;listen.group = www/listen.group = www/g' /usr/local/etc/php-fpm.d/www.conf
+sed -i '' -e 's/;listen.mode = 0660/listen.mode = 0600/g' /usr/local/etc/php-fpm.d/www.conf
+cp /usr/local/etc/php.ini-production /usr/local/etc/php.ini
+sed -i '' -e 's?;date.timezone =?date.timezone = "Universal"?g' /usr/local/etc/php.ini
+sed -i '' -e 's?;cgi.fix_pathinfo=1?cgi.fix_pathinfo=0?g' /usr/local/etc/php.ini
+git clone https://github.com/causefx/Organizr.git /usr/local/www/Organizr
 
-# Enable Service
-sysrc nzbget_enable=YES
-sysrc nzbget_conf_dir=/config
+# Set permissions
+chown -R www:www /usr/local/www /config
+
+# Enable the service
+sysrc nginx_enable=YES
+sysrc php_fpm_enable=YES
