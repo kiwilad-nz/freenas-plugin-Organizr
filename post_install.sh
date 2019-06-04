2d0c18e6cb5743be81f1a9d9f3d3339fb9aaa960
kiwilad-nz <50952231+kiwilad-nz@users.noreply.github.com>
2019-06-04 17:05:51 +1200

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -8,6 +8,8 @@ sed -i '' -e 's/;listen.mode = 0660/listen.mode = 0600/g' /usr/local/etc/php-fpm
 cp /usr/local/etc/php.ini-production /usr/local/etc/php.ini
 sed -i '' -e 's?;date.timezone =?date.timezone = "Universal"?g' /usr/local/etc/php.ini
 sed -i '' -e 's?;cgi.fix_pathinfo=1?cgi.fix_pathinfo=0?g' /usr/local/etc/php.ini
+
+# Clone Organizr git
 git clone https://github.com/causefx/Organizr.git /usr/local/www/Organizr
 
 # Set permissions
@@ -16,6 +18,9 @@ chown -R www:www /usr/local/www
 # Enable the service
 sysrc nginx_enable=YES
 sysrc php_fpm_enable=YES
+
+# Start the service
+service php-fpm start
 
 # Complete message - Print below text
 echo "Organizr Successfully Installed"
